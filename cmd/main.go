59c4c4e03f6fecca2780881c174be35f91f6ed36
404th <umarov.doniyor.2002@gmail.com>
2023-10-05 01:24:20 +0500

    basic bot is ready

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,55 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/404th/anonymous-letter/config"
+	"github.com/gin-gonic/gin"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	// 0: loading config
+	cfg := config.Load()
+
+	// 1: loading logging
+	lg := logrus.New()
+
+	switch cfg.Environment {
+	case config.DebugMode:
+		gin.SetMode(gin.DebugMode)
+	case config.TestMode:
+		gin.SetMode(gin.TestMode)
+	default:
+		gin.SetMode(gin.ReleaseMode)
+	}
+
+	// 2: connecting to telegram bot
+	bot, err := tgbotapi.NewBotAPI(cfg.BotToken)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s\n", bot.Self.UserName)
+	log.Printf("Authorized on Firstname account %s\n", &bot.Self.FirstName)
+	log.Printf("Authorized on Lastname account %s\n", &bot.Self.LastName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil { // If we got a message
+			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+			msg.ReplyToMessageID = update.Message.MessageID
+
+			bot.Send(msg)
+		}
+	}
 }
--- config/config.go
+++ config/config.go
@@ -1,1 +1,86 @@
 package config
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	"github.com/spf13/cast"
+)
+
+const (
+	// DebugMode indicates service mode is debug.
+	DebugMode = "debug"
+	// TestMode indicates service mode is test.
+	TestMode = "test"
+	// ReleaseMode indicates service mode is release.
+	ReleaseMode = "release"
+
+	ErrorModel = "!!!Error"
+	ErrorStyle = "--->"
+
+	// service name
+	SvcsUser = "UserService"
+
+	PostgresCtxTimeout = time.Second * 30       // postgres context timeout 30s
+	PostgresLogTime    = time.Millisecond * 500 // postgres context timeout 500ms
+)
+
+type Config struct {
+	ServiceName string
+	Environment string // debug, test, release
+	Version     string
+
+	BotToken string
+
+	PostgresHost           string
+	PostgresPort           int
+	PostgresUser           string
+	PostgresPassword       string
+	PostgresDatabase       string
+	PostgresMaxConnections int32
+
+	HTTPPort   string
+	HTTPScheme string
+}
+
+// Load ...
+func Load() Config {
+	envFileName := cast.ToString(getOrReturnDefaultValue("ENV_FILE_PATH", "./.env"))
+
+	if err := godotenv.Load(envFileName); err != nil {
+		fmt.Println("No .env file found")
+	}
+
+	config := Config{}
+
+	config.ServiceName = cast.ToString(getOrReturnDefaultValue("SERVICE_NAME", "anonymous_letter_postgres"))
+	config.Environment = cast.ToString(getOrReturnDefaultValue("ENVIRONMENT", DebugMode))
+	config.Version = cast.ToString(getOrReturnDefaultValue("VERSION", "1.0.0"))
+
+	config.BotToken = cast.ToString(getOrReturnDefaultValue("BOT_TOKEN", "123123abc"))
+
+	config.PostgresHost = cast.ToString(getOrReturnDefaultValue("POSTGRES_HOST", "0.0.0.0"))
+	config.PostgresPort = cast.ToInt(getOrReturnDefaultValue("POSTGRES_PORT", 5432))
+	config.PostgresUser = cast.ToString(getOrReturnDefaultValue("POSTGRES_USER", "postgres"))
+	config.PostgresPassword = cast.ToString(getOrReturnDefaultValue("POSTGRES_PASSWORD", "postgres"))
+	config.PostgresDatabase = cast.ToString(getOrReturnDefaultValue("POSTGRES_DATABASE", config.ServiceName))
+
+	config.HTTPPort = cast.ToString(getOrReturnDefaultValue("HTTP_PORT", "3000"))
+	config.HTTPScheme = cast.ToString(getOrReturnDefaultValue("HTTP_SCHEME", "http"))
+
+	config.PostgresMaxConnections = cast.ToInt32(getOrReturnDefaultValue("POSTGRES_MAX_CONNECTIONS", 80))
+
+	return config
+}
+
+func getOrReturnDefaultValue(key string, defaultValue interface{}) interface{} {
+	val, exists := os.LookupEnv(key)
+
+	if exists {
+		return val
+	}
+
+	return defaultValue
+}
